53412ef21239c4f66fc2fa0db9e018fd81ea1c2c
BrahiMouissi <brahim7khalil@gmail.com>
2024-08-01 16:38:18 +0100

    first commit

---
+++ src/app/__app.js
@@ -0,0 +1,12 @@
+import { UserProvider } from '../context/UserContext';
+import '../styles/globals.css';
+
+function App({ Component, pageProps }) {
+    return (
+        <UserProvider>
+            <Component {...pageProps} />
+        </UserProvider>
+    );
+}
+
+export default App;
